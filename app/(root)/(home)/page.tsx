af4c26d5fd572e9130a53b7ac3e67e97c62f82e3
eliellima00 <eliellimamatos@gmail.com>
2024-01-04 22:06:03 -0400

    feat: base layouts of QuestionCard

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,4 @@
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
 import NoResult from "@/components/shared/NoResult";
@@ -7,32 +8,40 @@ import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 
 const questions = [
-  // {
-  //   _id: 1,
-  //   title: "Cascading deletes in SQLAlchemy?",
-  //   tags: [
-  //     { _id: 1, name: "python" },
-  //     { _id: 2, name: "sql" },
-  //   ],
-  //   author: "John Doe",
-  //   upvotes: 10,
-  //   views: 100,
-  //   answers: 2,
-  //   createdAt: "2023-09-01-T12:00:00.000Z",
-  // },
-  // {
-  //   _id: 2,
-  //   title: "How to center a div?",
-  //   tags: [
-  //     { _id: 1, name: "HTML" },
-  //     { _id: 2, name: "CSS" },
-  //   ],
-  //   author: "John Doe",
-  //   upvotes: 10,
-  //   views: 100,
-  //   answers: 2,
-  //   createdAt: "2023-09-01-T12:00:00.000Z",
-  // },
+  {
+    _id: "1",
+    title: "Cascading deletes in SQLAlchemy?",
+    tags: [
+      { _id: "1", name: "python" },
+      { _id: "2", name: "sql" },
+    ],
+    author: {
+      _id: "1",
+      name: "John Doe",
+      picture: "url_to_picture",
+    },
+    upvotes: 10,
+    views: 100,
+    answers: [{}, {}],
+    createdAt: new Date("2023-09-01T12:00:00.000Z"),
+  },
+  {
+    _id: "2",
+    title: "How to center a div?",
+    tags: [
+      { _id: "1", name: "HTML" },
+      { _id: "2", name: "CSS" },
+    ],
+    author: {
+      _id: "1",
+      name: "John Doe",
+      picture: "url_to_picture",
+    },
+    upvotes: 10,
+    views: 100,
+    answers: [{}, {}],
+    createdAt: new Date("2023-09-01T12:00:00.000Z"),
+  },
 ];
 
 export default function Home() {
@@ -72,7 +81,19 @@ export default function Home() {
       <HomeFilters />
       <div className="mt-10 flex w-full flex-col gap-6">
         {questions.length > 0 ? (
-          questions.map((question) => "QuestionCard")
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
         ) : (
           <NoResult
             title="There's no questions to show"
