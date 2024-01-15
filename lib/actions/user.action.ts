a7d169b9f87b96e68809d2ba4897d342f339cf77
eliellima00 <eliellimamatos@gmail.com>
2024-01-14 21:40:59 -0400

    feat: add clerk webhooks to create, update and delete user

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,7 +1,11 @@
 "use server"
 
+import Question from "@/database/question.model";
 import User from "@/database/user.model";
+import console from "console";
+import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "../mongoose";
+import { CreateUserParams, DeleteUserParams, UpdateUserParams } from "./shared.types";
 
 export async function getUserById(params: any) {
   try {
@@ -14,6 +18,71 @@ export async function getUserById(params: any) {
     })
 
     return user
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase()
+
+    const newUser = await User.create(userData)
+    return newUser
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase()
+
+    const { clerkId, updateData, path } = params
+
+    await User.findOneAndUpdate({
+      clerkId
+    }, updateData, {
+      new: true
+    })
+
+    revalidatePath(path)
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    connectToDatabase()
+
+    const { clerkId } = params
+
+    const user = await User.findOneAndDelete({
+      clerkId
+    })
+
+    if (!user) {
+      throw new Error('User not found')
+    }
+
+    // delete user from datase 
+    // and questions, answersm comments, etc.
+
+    const userQuestionIds = await Question.find({
+      author: user.id
+    }).distinct('_id')
+
+    await Question.deleteMany({
+      author: user.id
+    })
+
+    // TODO: Delete all user answers, comments, etc...
+    const deletedUser = await User.findByIdAndDelete(user._id);
+    return deleteUser;
   } catch (error) {
     console.log(error)
     throw error
