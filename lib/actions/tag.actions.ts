4d63699d6a0bb946decaf9c7427ebb808e86b067
eliellima00 <eliellimamatos@gmail.com>
2024-01-23 19:34:39 -0400

    feat: add tags page

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -1,8 +1,9 @@
 "use server"
 
+import Tag from "@/database/tag.model"
 import User from "@/database/user.model"
 import { connectToDatabase } from "../mongoose"
-import { GetTopInteractedTagsParams } from "./shared.types"
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types"
 
 export async function getTopInterectedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -30,6 +31,19 @@ export async function getTopInterectedTags(params: GetTopInteractedTagsParams) {
         name: 'tag3'
       }
     ]
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase()
+
+    const tags = await Tag.find({})
+
+    return { tags }
   } catch (error) {
     console.log(error)
     throw error
